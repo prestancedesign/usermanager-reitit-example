6a5cdf507b9afd998da78c4ebad7f62491089a77
Michael SALIHI <admin@prestance-informatique.fr>
2020-04-20 16:05:25 +0200

    Alias to add dev directory in path and Emacs dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,7 @@
-((nil . ((cider-ns-refresh-before-fn . "integrant.repl/suspend")
-         (cider-ns-refresh-after-fn  . "integrant.repl/resume"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((clojure-mode
+  (cider-ns-refresh-before-fn . "integrant.repl/suspend")
+  (cider-ns-refresh-after-fn . "integrant.repl/resume")
+  (cider-clojure-cli-global-options . "-A:dev")))
